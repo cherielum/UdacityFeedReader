48ddc1c439d35802667a2791f1eedcf6f2edec7f
Cherie Lum <charismatic.cherie@gmail.com>
2018-08-24 15:25:01 -0400

    The menu, Intial Enteries tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,8 +61,8 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
-        it('if it is hidden', function() {
+    describe('The menu', function() {
+        it('is hidden', function() {
             const body = document.querySelector('body');
             expect(body.classList.contains('menu-hidden')).toBe(true);
         });
@@ -73,19 +73,58 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
 
+        it('toggles on and off', function() {
+            const body = document.querySelector('body');
+            const menu = document.querySelector('.menu-icon-link');
+            menu.click();
+            expect(body.classList.contains('menu-hidden)')).toBe(false);
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+        });
+
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    describe('Initial Entries', function() {
+    /* TODO: Write a test that ensures when the loadFeed
+    * function is called and completes its work, there is at least
+    * a single .entry element within the .feed container.
+    * Remember, loadFeed() is asynchronous so this test will require
+    * the use of Jasmine's beforeEach and asynchronous done() function.
+    */
+
+
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('completes work', function() {
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length > 0).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Section', function() {
+        const feed = document.querySelector('.feed');
+        const firstFeed = [];
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-}());
+
+        beforeEach(function(done) {
+            loadFeed(0);
+            // console.log(feed.children[0].innerText);
+            Array.from(feed.children).forEach(function(entry) {
+                firstFeed.push(entry.innerText);
+            });
+            loadFeed(1,done);
+        });
+
+        it('content changes', function() {
+            console.log(feed.children[0].innerText);
+        });
+
+    })
+})
